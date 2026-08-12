3270241d86b2d784b14033a3a8c88a9010cb96f1
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-08-12 22:19:15 +0530

    comming are added here

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             {children}
           </>
         ) : (
-          <div className="w-full h-screen text-center flex justify-center items-center "><div>Coming soon....</div></div>
+          <div className="w-full h-screen text-center flex justify-center items-center text-white"><div>Coming soon....</div></div>
         )}
       </body>
     </html>
--- components/LandingPage/Hero/HeroRightSection.tsx
+++ components/LandingPage/Hero/HeroRightSection.tsx
@@ -1,8 +1,11 @@
+import ButtonNormal from "@/components/ui/Buttons/ButtonNormal";
 import React from "react";
 
 const HeroRightSection:React.FC  = ()=>{
     return (
-        <div className="w-[70%]">Cardes</div>
+        <div className="w-[70%] flex flex-col">
+            {/* <ButtonNormal title={}/> */}
+        </div>
     )
 }
 
