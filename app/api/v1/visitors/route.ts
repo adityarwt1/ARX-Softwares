fac7b49d8beacdfd9b404ce8b823be082015703e
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-08-12 22:46:41 +0530

    ip address to location are added here

--- app/api/v1/visitors/route.ts
+++ app/api/v1/visitors/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
+
 export async function GET(req: NextRequest) {
   const forwardedFor = req.headers.get("x-forwarded-for");
 
@@ -8,11 +9,31 @@ export async function GET(req: NextRequest) {
     req.headers.get("x-real-ip") ||
     "unknown";
 
-  console.log("Client IP:", ip);
+  if (ip === "unknown") {
+    return NextResponse.json(
+      { success: false, message: "IP not found" },
+      { status: 400 }
+    );
+  }
+
+  const geoResponse = await fetch(
+    `http://ip-api.com/json/${ip}?fields=status,message,country,regionName,city,lat,lon,timezone,isp,query`
+  );
+
+  const geo = await geoResponse.json();
 
   return NextResponse.json({
     success: true,
     ip,
+    location: {
+      country: geo.country,
+      region: geo.regionName,
+      city: geo.city,
+      latitude: geo.lat,
+      longitude: geo.lon,
+      timezone: geo.timezone,
+      isp: geo.isp,
+    },
   });
 }
 export async function POST(req:Request) {
