6c003a8886de0e33a16efef680d9e41393c85a76
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-08-15 00:04:56 +0530

    working on the signup route:

--- app/api/v1/auth/user/signup/route.ts
+++ app/api/v1/auth/user/signup/route.ts
@@ -1,12 +1,16 @@
 import { HTTP_Response } from "@/interfaces/httpResponse/httpServerResponse";
-import { internalServerIssue } from "@/utils/httpResponses";
+import { internalServerIssue, resultantResponse } from "@/utils/httpResponses";
 import { NextRequest, NextResponse } from "next/server";
 interface CreateUserResponse {
     accessToken:string
 }
 export async function POST(req:NextRequest):Promise<NextResponse< HTTP_Response<CreateUserResponse>>> {
     try {
-        
+        return resultantResponse<CreateUserResponse>({
+            data:{
+                accessToken:"DUMMY"
+            }
+        })
     } catch (error) {
         console.log(error)
         return internalServerIssue<CreateUserResponse>({
